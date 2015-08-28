8a63fa1b6cb9728d4e733bd6d088f387ffb0f1bd
Vicent <huanggang1109@163.com>
2015-08-28 16:23:12 +0800

    sdgdfsg

---
+++ TestGit/dsg.h
@@ -0,0 +1,13 @@
+//
+//  dsg.h
+//  TestGit
+//
+//  Created by Vicent on 15/8/28.
+//  Copyright (c) 2015年 Vicent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dsg : UIView
+
+@end
---
+++ TestGit/dsg.m
@@ -0,0 +1,21 @@
+//
+//  dsg.m
+//  TestGit
+//
+//  Created by Vicent on 15/8/28.
+//  Copyright (c) 2015年 Vicent. All rights reserved.
+//
+
+#import "dsg.h"
+
+@implementation dsg
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
